51ff1498e4a9cec0cefa2748c7d4111583cba4af
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-09-02 17:36:44 -0300

    Rename livros.json to itens.json and update references
    Renamed the data file from livros.json to itens.json and updated all references in teste.js to use the new filename. This improves clarity and better reflects the contents of the file.

--- teste.js
+++ teste.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
-const dadosAtuais = JSON.parse(fs.readFileSync('livros.json'))
+const dadosAtuais = JSON.parse(fs.readFileSync('itens.json'))
 const novoDado = { id: '4', nome: 'Capuz de Rabadon'}
 
-fs.writeFileSync('livros.json', JSON.stringify([...dadosAtuais, novoDado]))
+fs.writeFileSync('itens.json', JSON.stringify([...dadosAtuais, novoDado]))
 
-console.log(JSON.parse(fs.readFileSync('livros.json')))
+console.log(JSON.parse(fs.readFileSync('itens.json')))
