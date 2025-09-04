35757fc620f87be70e4bbc2fe19ee7998c69f7f9
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-09-04 17:35:05 -0300

    Add comments to item routes for clarity
    Added comments to the item routes in routes/item.js to clarify the purpose of each route, specifying which endpoints return all items and which return a specific item by id.

--- routes/item.js
+++ routes/item.js
@@ -2,9 +2,9 @@ const { Router } = require('express')
 const { getItens, getItem, postItem } = require('../controllers/item')
 
 const router = Router()
-
+// GET --> todos os itens
 router.get('/', getItens)
-
+// GET --> um item especificado pelo id
 router.get('/:id', getItem)
 
 router.post('/', postItem)
