1ee49c404610da906af17309f25c341328609738
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-09-05 20:00:46 -0300

    configuring cors and axios

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const rotaItem = require('./routes/item')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors({ origin: '*' }))
 
 app.use('/itens', rotaItem)
 
-const port = 3000
+const port = 3000;
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
