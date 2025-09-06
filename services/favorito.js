317941ec2ab7c53acd408d831079a7d8dc930aba
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-09-06 11:01:13 -0300

    new dataset created 'favoritos.json' and service to work with him

---
+++ services/favorito.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const { deletarItemPorId } = require('./itens')
+
+function getTodosFavoritos() {
+    return JSON.parse(fs.readFileSync('favoritos.json'))
+};
+
+function deletarFavoritoId() {
+    const itens = JSON.parse(fs.readFileSync('favoritos.json'))
+    const itensFiltrados = itens.filter(item => item.id !== id)
+    fs.writeFileSync('favoritos.json', JSON.stringify(itensFiltrados))
+}
+
+function insereFavorito(id) {
+    const itens = JSON.parse(fs.readFileSync('itens.json'))
+    const favoritos = JSON.parse(fs.readFileSync('favoritos.json'))
+
+    const itemInserido = itens.find(item => item.id === id)
+    const novaListaFavoritos = [...favoritos, itemInserido]
+
+    fs.writeFileSync('favoritos.json', JSON.stringify(novaListaFavoritos))
+
+}
+
+
+module.exports = {
+    getTodosFavoritos,
+    deletarItemPorId,
+    insereFavorito
+}
